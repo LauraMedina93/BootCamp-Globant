bed0e5eeb0f3dcd52701526547b6b3e391f96968
Usuario <Usuario@Laura>
2015-11-23 13:10:44 -0300

    topic 0 (files and sing up) style.css, script.js, index.html and todo list modified.-

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
     $("section").fadeIn("6000");
     $(".alias").focus();
-
+    
+     $(".button1").click(function() {
+        alert("Welcome!")
+    });
+    //welcome+name
     $("button").click(function() {
         var value = $(".alias").val();
         $.ajax({
@@ -11,27 +15,25 @@ $(document).ready(function() {
                 $("section").html(data.response)
             },
             error: function(error) {
-                 $("section").toggleClass("error-txt")
+                $("section").toggleClass("error-txt")
             }
         });
-        
+        //search on spotify
         $.ajax({
             url: "https://api.spotify.com/v1/search?q=Rolling+Stones&type=album",
             type: 'GET',
             success: function(data) {
                 $("section").html(data.response)
                 console.log(data.albums);
-                for (var i= 0 ; i< data.albums.items.length; i++){
-                    
+                for (var i = 0; i < data.albums.items.length; i++) {
+
                 }
             },
             error: function(error) {
-                 alert(error);
+                alert(error);
             }
         });
-        });
-
-
-
     });
+   
+});
 
