b47fbb64d99460249bbd6784846f7efe6923a27b
Usuario <Usuario@Laura>
2015-12-03 13:02:49 -0300

    javascript from topic_2

--- topics/topic_2/index.js
+++ topics/topic_2/index.js
@@ -1,28 +1,95 @@
-//exercises - topic2
+$(document).ready(function() {
 
-function Movie( title, year, gender ) { //class Movie
+    var shareWith = {share: function(friendName) {
+            console.log("Sharing " + this.title + " with " + friendName);
+        }
+    };
+    var like1 = {likes: function(friendName) {
+            console.log(friendName + " likes " + this.title);
+        }
+    };
+
+
+    function Movie() {
+        this.play = function() {
+            console.log("Playing: " + this.title) + (". Enjoy!!!");
+        };
+        this.stop = function() {
+            console.log(("The film ") + this.title + " was stopped.");
+        };
+        this.set = function(attr, value) {
+            this[attr] = value;
+        };
+        this.get = function(attr) {
+            return this[attr];
+        };
+        this.share = shareWith.share;
+        this.like = like1.likes;
+        this.arrayOfActors = arrayActors;
+    }
+    ;
+
+    function actor() {
+        this.name;
+        this.age;
+
+        this.set = function(name, age) {
+            this.name = name;
+            this.age = age;
+        };
+    }
+    ;
+
+
+    var brad = new actor();
+    brad.set("Brad Pitt", 45);
+    var vin = new actor();
+    vin.set("Vin Diesel", 48);
+    var arrayActors = [brad, vin];
+
+
+    var MovieObserver = document;
+    var term = new Movie();
+    term.set("title", "Terminator");
+    term.share("Laura Medina");
+    term.like("Viviana Czenczek");
+
+
+    var play = new Event("playing");
+    var stop = new Event("stopped");
+
+
+    MovieObserver.addEventListener("playing", function() {
+        $("h3").text("Playing this film");
+        console.log("Playing");
+    }, false);
+
+    MovieObserver.addEventListener("stopped", function() {
+        $("h3").text("The film was stopped");
+        console.log("Stopped")
+    }, false);
+
+
+    $(".butt-play").click(function() {
+        MovieObserver.dispatchEvent(play);
+    });
+    $(".butt-stop").click(function() {
+        MovieObserver.dispatchEvent(stop);
+    });
+
+
+    function DownloadableMovie() {
+        Movie.call(this);
+    }
+    ;
+    DownloadableMovie.prototype = new Movie();
+    
+
+    var downFilm = new DownloadableMovie();
+    downFilm.set("title", "Iron Man 2");
+    downFilm.play();
  
-  this.title = title;
-  this.year = year;
-  this.gender = gender;
-
-}
-
-Movie.prototype.play = function () { 
-  return this.title + " is playing... This film is from " + this.year + " and is a one of "+ this.gender + ". Enjoy!";
-};
-Movie.prototype.stop = function(){
-    return this.title + ": this film was stopped";
-};
-
-var transformers = new Movie ("Transformers", "2014", "Action");
-var insidious = new Movie ("Insidious", "2010", "Terror");
-
-console.log(insidious.play());
-console.log(transformers.stop());
-
-function DownloadableMovie(){
-    Movie.call(this);
-}
 
 
+
+}); 
